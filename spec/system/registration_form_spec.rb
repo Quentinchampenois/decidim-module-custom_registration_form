f80c54c9218352359a93c90932976f88cc5c2971
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2022-03-29 10:48:16 +0200

    Add registration specs and fix rubocop offenses

---
+++ spec/system/registration_form_spec.rb
@@ -0,0 +1,105 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+def fill_registration_form
+  fill_in :registration_user_name, with: "Nikola Tesla"
+  fill_in :registration_user_nickname, with: "the-greatest-genius-in-history"
+  fill_in :registration_user_email, with: "nikola.tesla@example.org"
+  fill_in :registration_user_password, with: "sekritpass123"
+  fill_in :registration_user_password_confirmation, with: "sekritpass123"
+end
+
+describe "Registration", type: :system do
+  let!(:organization) { create(:organization) }
+  let!(:terms_and_conditions_page) { Decidim::StaticPage.find_by(slug: "terms-and-conditions", organization: organization) }
+
+  before do
+    Decidim::ContentBlock.create(decidim_organization_id: organization.id, weight: 1, scope_name: "signup_form", images: {}, manifest_name: "gender", published_at: Time.zone.now, settings: nil, scoped_resource_id: nil)
+
+    switch_to_host(organization.host)
+    visit decidim.new_user_registration_path
+  end
+
+  context "when signing up" do
+    describe "on first sight" do
+      it "shows fields empty" do
+        expect(page).to have_content("Sign up to participate")
+        expect(page).to have_field("registration_user_name", with: "")
+        expect(page).to have_field("registration_user_nickname", with: "")
+        expect(page).to have_field("registration_user_email", with: "")
+        expect(page).to have_field("registration_user_password", with: "")
+        expect(page).to have_field("registration_user_password_confirmation", with: "")
+        expect(page).to have_field("registration_user_newsletter", checked: false)
+      end
+    end
+
+    describe "on cached sight with a different language", :caching do
+      it "shows the omniauth buttons in correct locale" do
+        expect(page).to have_content("Sign in with Facebook")
+
+        within_language_menu do
+          click_link "Català"
+        end
+
+        expect(page).to have_content("Inicia sessió amb Facebook")
+      end
+    end
+  end
+
+  context "when newsletter checkbox is unchecked" do
+    it "opens modal on submit" do
+      within "form.new_user" do
+        find("*[type=submit]").click
+      end
+      expect(page).to have_css("#sign-up-newsletter-modal", visible: :visible)
+      expect(page).to have_current_path decidim.new_user_registration_path
+    end
+
+    it "checks when clicking the checking button" do
+      within "form.new_user" do
+        find("*[type=submit]").click
+      end
+      click_button "Check and continue"
+      expect(page).to have_current_path decidim.new_user_registration_path
+      expect(page).to have_css("#sign-up-newsletter-modal", visible: :all)
+      expect(page).to have_field("registration_user_newsletter", checked: true)
+    end
+
+    it "submit after modal has been opened and selected an option" do
+      within "form.new_user" do
+        find("*[type=submit]").click
+      end
+      click_button "Keep unchecked"
+      expect(page).to have_css("#sign-up-newsletter-modal", visible: :all)
+      fill_registration_form
+      within "form.new_user" do
+        find("*[type=submit]").click
+      end
+      expect(page).to have_current_path decidim.user_registration_path
+      expect(page).to have_field("registration_user_newsletter", checked: false)
+    end
+  end
+
+  context "when newsletter checkbox is checked but submit fails" do
+    before do
+      fill_registration_form
+      page.check("registration_user_newsletter")
+    end
+
+    it "keeps the user newsletter checkbox true value" do
+      within "form.new_user" do
+        find("*[type=submit]").click
+      end
+      expect(page).to have_current_path decidim.user_registration_path
+      expect(page).to have_field("registration_user_newsletter", checked: true)
+    end
+  end
+
+  describe "when registration form is enabled" do
+    it "display registration form block" do
+      expect(page).to have_content("Extra registration form fields")
+      expect(page).to have_content("Select your gender")
+    end
+  end
+end
